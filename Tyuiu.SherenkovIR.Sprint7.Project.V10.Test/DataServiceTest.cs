d1624c2d63da97ff3c239ed885497113c2e552ce
Sherenkov Ivan <sherenkov12@gmail.com>
2025-12-20 16:00:16 +0500

    Project

--- Tyuiu.SherenkovIR.Sprint7.Project.V10.Test/DataServiceTest.cs
+++ Tyuiu.SherenkovIR.Sprint7.Project.V10.Test/DataServiceTest.cs
@@ -5,19 +5,16 @@ namespace Tyuiu.SherenkovIR.Sprint7.Project.V10.Test
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void ValidGetMatrix()
+        public void TestFilter()
         {
-            DataService ds = new DataService();
-            string pathsave = Path.Combine("C:", "DataSprint7", "TEST.csv");
-            using (StreamWriter writer = new StreamWriter(pathsave))
-            {
-                writer.WriteLine("1;8");
-                writer.WriteLine("0;5");
-                writer.WriteLine("4;9");
-            }
-            string[,] res = ds.GetMatrix(pathsave);
-            string[,] wait = { { "1", "8" }, { "0", "5" }, { "4", "9" } };
-            CollectionAssert.AreEqual(res, wait);
+            var orderManager = new OrderManager();
+            orderManager.AddOrder(new Order { OrderNumber = "1", LastName = "Vorobiev", FirstName = "Sergey", MiddleName = "Leonidovich", Index = "123456", City = "Moscow", Address = "Azovskogo 6", PhoneNumber = "8952755555", OrderDate = DateTime.Now, OrderName = "ProductA", Price = 100, Quantity = 2, AccountNumber = "01" });
+            orderManager.AddOrder(new Order { OrderNumber = "2", LastName = "Ivanov", FirstName = "Ivan", MiddleName = "Ivanovich", Index = "654321", City = "Tyumen", Address = "Lenina 34", PhoneNumber = "8952766666", OrderDate = DateTime.Now, OrderName = "ProductB", Price = 400, Quantity = 1, AccountNumber = "02" });
+
+            var filteredOrders = orderManager.FilterByPrice(100, 300);
+
+            Assert.AreEqual(1, filteredOrders.Count);
+            Assert.AreEqual("Vorobiev", filteredOrders[0].LastName);
         }
 
     }
