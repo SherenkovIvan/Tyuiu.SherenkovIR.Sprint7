d7a390987e612974c51b0821f945aff13bd8641f
Sherenkov Ivan <sherenkov12@gmail.com>
2025-12-23 01:20:07 +0500

    Project

--- Tyuiu.SherenkovIR.Sprint7.Project.V10/FormMain_SIR.Designer.cs
+++ Tyuiu.SherenkovIR.Sprint7.Project.V10/FormMain_SIR.Designer.cs
@@ -32,9 +32,9 @@ namespace Tyuiu.SherenkovIR.Sprint7.Project.V10
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain_SIR));
             dataGridViewOrder_SIR = new DataGridView();
             buttonAddOrder_SIR = new Button();
@@ -198,16 +198,16 @@ namespace Tyuiu.SherenkovIR.Sprint7.Project.V10
             // 
             // chartOrder_SIR
             // 
-            chartArea2.Name = "ChartArea1";
-            chartOrder_SIR.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            chartOrder_SIR.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            chartOrder_SIR.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chartOrder_SIR.Legends.Add(legend1);
             chartOrder_SIR.Location = new Point(0, 394);
             chartOrder_SIR.Name = "chartOrder_SIR";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            chartOrder_SIR.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chartOrder_SIR.Series.Add(series1);
             chartOrder_SIR.Size = new Size(425, 159);
             chartOrder_SIR.TabIndex = 13;
             chartOrder_SIR.Text = "chart1";
